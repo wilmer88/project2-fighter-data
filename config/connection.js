11e9f97c13a979e8c13d82d79e23a40b587a81e2
wilmerbaby <wilmerbaby@gmail.com>
2020-10-16 11:17:50 -0400

    sequelize test

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,15 @@
-var mysql = require("mysql");
+const Sequelize = require("sequelize");
 
-var connection = mysql.createConnection({
+const sequelize = new Sequelize ("pfs", "root", "morter706", {
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "morter706",
-  database: "pfs"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
-  console.log("connected as id " + connection.threadId);
-});
+})
 
 // Export connection for our ORM to use.
-module.exports = connection;
+module.exports = sequelize;
